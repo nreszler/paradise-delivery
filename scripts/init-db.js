ec9cac6362f6982f3862de2666321aa2506a14b0
Nathan Reszler <wwqqwat@gmail.com>
2026-02-23 01:40:23 -0800

    Fix: Correct database and schema paths for Render deployment

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -2,8 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'paradise.db');
-const schemaPath = path.join(__dirname, 'schema.sql');
+const dbPath = path.join(__dirname, '..', 'paradise.db');
+const schemaPath = path.join(__dirname, '..', 'database', 'schema.sql');
 
 console.log('🔄 Initializing database...');
 
