0df6021cf2129e2f442f145dcbcadafc8471b2da
Nathan Reszler <wwqqwat@gmail.com>
2026-02-23 02:44:15 -0800

    ULTRA SIMPLE: Bare bones HTTP server, no dependencies

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('STARTING');
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Paradise Delivery is running!\n');
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log('RUNNING ON PORT', port);
+});
